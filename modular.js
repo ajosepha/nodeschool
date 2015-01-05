920131a54e9dc51ab81f84e452ac1960a38e3dc8
Ariel Isaacs <ariel.isaacs@gmail.com>
2015-01-05 13:55:32 -0800

    not working yet

--- modular.js
+++ modular.js
@@ -2,5 +2,20 @@ var fs = require('fs')
 var path = require('path')
 
 module.exports = function(dirname, ext, callback){
-	extension = "." + ext
+	var extension = "." + ext
+	var results = [];
+	fs.readdir(dirname, function(err, list){
+		if(err){
+			callback(null, err)
+		}
+		else{
+
+			list.forEach(function(file){
+				if(path.extname(file)== extension){
+					results.push(file)
+				};
+			});
+			callback(null, results)
+		}
+	})
 }
--- module1.js
+++ module1.js
@@ -1,1 +1,9 @@
 require('./modular.js')
+var dirname = process.argv[2]
+var ext = process.argv[3]
+
+modular(dirname, ext function(err, filenames){
+	for(i = 0; i < filenames.length; i++){
+		console.log(filenames[i])
+	}
+})
