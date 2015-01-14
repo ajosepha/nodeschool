c48844be976d0a28c30dfd8754f7ee15de5482c5
Ariel Isaacs <ariel.isaacs@gmail.com>
2015-01-13 23:45:42 -0800

    test

--- babySteps.js
+++ babySteps.js
@@ -1,6 +1,6 @@
 // Write a program that accepts one or more numbers as command-line arguments and prints the sum of those numbers to the console (stdout).
 
-// process.argv is an array
+
 
 var result = 0
 for(var i = 2; i < process.argv.length; i++){
